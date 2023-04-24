71e96e8bed0d7b7e20d9fae2134bf992a7e8caf1
jumettan <juho.mettanen@student.samk.fi>
2023-04-24 18:13:55 +0300

    changes
    player events still need some work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from typing import List, Optional, Union
-from models import Player
-from databases import Database
+from typing import List
+from models import Player, Event
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from sqlalchemy import (
@@ -50,7 +49,7 @@ async def get_player(id: int):
     player = next((p for p in players if p.id == id), None)
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
-    player_events = [e for e in events if e["player_id"] == id]
+    player_events = [e for e in events if e.player_id == id]
     return {"id": player.id, "name": player.name, "events": player_events}
 
 class EventCreate(BaseModel):
@@ -65,13 +64,19 @@ async def create_event(event_create: EventCreate, player_id: int):
     player = next((p for p in players if p.id == player_id), None)
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
-    new_event = {
-        "id": len(events) + 1,
-        "type": event_create.type,
-        "detail": event_create.detail,
-        "timestamp": str(datetime.now()),
-        "player_id": player_id,
-    }
+    new_event = Event(id=len(events) + 1, type=event_create.type, detail=event_create.detail, timestamp=datetime.now(), player_id=player_id)
+    player.events.append(new_event)
     events.append(new_event)
-    return {"id": new_event["id"]}
+    return {"id": new_event.id}
 
+@app.get("/players/{id}/events", response_model=List[Event])
+async def get_player_events(id: int, type: str = None):
+    player = next((p for p in players if p.id == id), None)
+    if not player:
+        raise HTTPException(status_code=404, detail="Player not found")
+    if type and type not in Event.Types:
+            raise HTTPException(status_code=400, detail="Invalid event type")
+    player_events = player.events
+    if type:
+        player_events = [e for e in player_events if e.type == type]
+    return [e.dict(exclude={"type", "detail", "player_id", "TYPES"}) for e in player_events]
--- models.py
+++ models.py
@@ -1,16 +1,81 @@
-from typing import List, Optional
+from typing import List
+from fastapi import HTTPException, Query
+from datetime import datetime
 from pydantic import BaseModel
+
 
 class PlayerBase(BaseModel):
     name: str
 
+
 class PlayerCreate(PlayerBase):
     pass
 
+
 class Player(PlayerBase):
     id: int
-
-
+    events: List["Event"] = []
 
     class Config:
         orm_mode = True
+
+
+class EventBase(BaseModel):
+    type: str
+    detail: str
+
+
+class EventCreate(EventBase):
+    pass
+
+class Event(BaseModel):
+    id: int
+    type: str
+    detail: str
+    timestamp: datetime
+    player_id: int
+    
+    Types = ["level_started", "level_solved"]
+    
+    class Config:
+        orm_mode = True
+
+
+Player.update_forward_refs()
+
+players = {
+    1: Player(id=1, name="Reijo", events=[Event(id=1, type="level_started", detail="level_1212_001", timestamp=datetime(2023, 1, 13, 12, 1, 22), player_id=1)])
+}
+# GET /players/{id}
+def get_player(id: int):
+    if id not in players:
+        raise HTTPException(status_code=404, detail="Player not found")
+    player = players[id]
+    events = [{"id": e.id, "type": e.type, "detail": e.detail, "timestamp": e.timestamp, "player_id": e.player_id} for e in player.events]
+    return {"id": player.id, "name": player.name, "events": events}
+
+# POST /events?player_id={player_id}
+def create_event(player_id: int, event: EventCreate):
+    if player_id not in players:
+        raise HTTPException(status_code=404, detail="Player not found")
+    event_dict = event.dict()
+    event_dict["id"] = len(players[player_id].events) + 1
+    event_dict["timestamp"] = datetime.now()
+    event_dict["player_id"] = player_id
+    players[player_id].events.append(Event(**event_dict))
+    return {"message": "Event created successfully"}
+
+# GET /players/{id}/events
+def get_player_events(id: int, type: str = None):
+    if id not in players:
+        raise HTTPException(status_code=404, detail="Player not found")
+    player = players[id]
+    if not player.events:
+        return []
+    if type:
+        if type not in [e.type for e in player.events]:
+            raise HTTPException(status_code=400, detail="Invalid event type")
+        events = [{"id": e.id, "type": e.type, "detail": e.detail, "timestamp": e.timestamp, "player_id": e.player_id} for e in player.events if e.type == type]
+    else:
+        events = [{"id": e.id, "type": e.type, "detail": e.detail, "timestamp": e.timestamp, "player_id": e.player_id} for e in player.events]
+    return events
